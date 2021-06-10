e1a6c10e467917f45d895de43e0b9166b2838783
harishpamnanijaipur <85674702+harishpamnanijaipur@users.noreply.github.com>
2021-06-10 16:32:22 +0530

    Add files via upload

---
+++ Student_Activity_G12_C1.py
@@ -0,0 +1,43 @@
+import pygame
+
+#initialising pygame and its functions 
+pygame.init()
+
+# creating game window and title
+screen = pygame.display.set_mode((400,600))
+pygame.display.set_caption("Space Invaders")
+
+# Display game window 
+background_image = pygame.image.load("bg2.jpg").convert()
+screen.fill((0,0,0))
+
+
+
+#Creating Players 
+
+enemy_image = pygame.image.load("enemy.png").convert_alpha()
+
+enemy=pygame.Rect(300,300,30,30)
+
+
+screen.blit(enemy_image , enemy)
+pygame.display.update()
+
+
+
+crashed = False
+
+
+while not crashed:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        print(event)
+
+    pygame.display.update()
+
+    
+pygame.quit()
+#quit()    
